3b73ddcc2c9c99973d7f459598dd27c0ac80580b
AntoineTA <an.taillard@gmail.com>
2024-02-18 21:52:05 +0000

    implemented message log

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from main.models import User
+from .models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     groups = ['chat']
@@ -11,21 +14,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        print(message)
+        author = await database_sync_to_async(User.objects.get)(username=text_data_json['author'])
+
+        await database_sync_to_async(Message.objects.create)(author=author, content=message)
 
         await self.channel_layer.group_send(
             'chat',
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'author': author.username
             }
         )
 
     # Receive message from group
     async def chat_message(self, event):
-        message = event['message']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': event['message'],
+            'author': event['author']
         }))
---
+++ chat/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from main.models import User
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
