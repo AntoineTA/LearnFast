63cd0993161a3ed4c8015879e7c45f3f20ad2a1e
AntoineTA <an.taillard@gmail.com>
2024-02-18 22:29:43 +0000

    display message history

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Message
 
 class IndexView(TemplateView):
     template_name = 'chat/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Fetch the last 10 messages and add them to the context
+        messages = Message.objects.order_by('-timestamp')[:10]
+        context['messages'] = messages
+
+        return context
