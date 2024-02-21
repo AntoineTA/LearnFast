193418cb49304b43afade958107edcce2f519650
AntoineTA <an.taillard@gmail.com>
2024-02-21 21:36:34 +0000

    implement basic permissions

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
+from .serializers import *
+from .permissions import *
 from main.models import *
-from . import serializers
 from rest_framework import generics
 
 class UserList(generics.ListAPIView):
+    permission_classes = [IsTeacher]
+    
     queryset = User.objects.all()
-    serializer_class = serializers.UsersSerializer
+    serializer_class = UsersSerializer
